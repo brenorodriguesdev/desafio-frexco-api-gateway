14611bb5564db9faa8269467dfe9123b109f8465
“Breno <brenorodriguesdev@gmail.com>
2021-12-09 12:13:47 -0300

    feat: add criar produto service

---
+++ src/data/services/produto/criar-produto.ts
@@ -0,0 +1,15 @@
+import { CriarProdutoModel } from "../../../domain/models/produto/criar-produto";
+import { ProdutoModel } from "../../../domain/models/produto/produto";
+import { CriarProdutoUseCase } from "../../../domain/useCases/produto/criar-produto";
+import { AMQPClient } from "../../contracts/amqp-client";
+
+export class CriarProdutoService implements CriarProdutoUseCase {
+    constructor (private readonly amqpClient: AMQPClient) {}
+    async criar(data: CriarProdutoModel): Promise<ProdutoModel| Error> {
+        const produto = await this.amqpClient.send('criar-produto', data)
+        if (produto['error']) {
+            return new Error(produto['error'])
+        }
+        return produto
+    }
+}
