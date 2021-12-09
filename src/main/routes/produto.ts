e0ac8c3ad8aa2becf7d26b6d17cf0c4e6b5bd939
“Breno <brenorodriguesdev@gmail.com>
2021-12-09 13:02:11 -0300

    feat: add produto routes

---
+++ src/main/routes/produto.ts
@@ -0,0 +1,13 @@
+import { Router } from "express"
+import { adaptRouter } from "../adapters/express-controller"
+import { makeAtualizarProdutoController } from "../factories/controllers/produto/atualizar-produto"
+import { makeCriarProdutoController } from "../factories/controllers/produto/criar-produto"
+import { makeDeletarProdutoController } from "../factories/controllers/produto/deletar-produto"
+import { makeListarProdutosController } from "../factories/controllers/produto/listar-produtos"
+
+export default (router: Router): void => {
+    router.put('/atualizarProduto', adaptRouter(makeAtualizarProdutoController()))
+    router.post('/criarProduto', adaptRouter(makeCriarProdutoController()))
+    router.delete('/deletarProduto/:id', adaptRouter(makeDeletarProdutoController()))
+    router.get('/listarProdutos', adaptRouter(makeListarProdutosController()))
+}
