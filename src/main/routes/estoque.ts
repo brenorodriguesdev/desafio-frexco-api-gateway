2d18d0ec755d3ebcabbe66000f1ffd12ff80ad86
“Breno <brenorodriguesdev@gmail.com>
2021-12-09 13:02:56 -0300

    feat: add estoque routes

---
+++ src/main/routes/estoque.ts
@@ -0,0 +1,13 @@
+import { Router } from "express"
+import { adaptRouter } from "../adapters/express-controller"
+import { makeAtualizarEstoqueController } from "../factories/controllers/estoque/atualizar-estoque"
+import { makeCriarEstoqueController } from "../factories/controllers/estoque/criar-estoque"
+import { makeDeletarEstoqueController } from "../factories/controllers/estoque/deletar-estoque"
+import { makeListarEstoquesController } from "../factories/controllers/estoque/listar-estoques"
+
+export default (router: Router): void => {
+    router.put('/atualizarEstoque', adaptRouter(makeAtualizarEstoqueController()))
+    router.post('/criarEstoque', adaptRouter(makeCriarEstoqueController()))
+    router.delete('/deletarEstoque/:id', adaptRouter(makeDeletarEstoqueController()))
+    router.get('/listarEstoques', adaptRouter(makeListarEstoquesController()))
+}
