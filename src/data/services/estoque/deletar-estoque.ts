3762c1e6fb9bcbfa2098a838e098a201e98c0231
“Breno <brenorodriguesdev@gmail.com>
2021-12-09 12:18:55 -0300

    feat: add deletar estoque service

--- src/data/services/estoque/deletar-estoque.ts
+++ src/data/services/estoque/deletar-estoque.ts
@@ -1,10 +1,10 @@
-import { DeletarProdutoUseCase } from "../../../domain/useCases/produto/deletar-produto";
+import { DeletarEstoqueUseCase } from "../../../domain/useCases/estoque/deletar-estoque";
 import { AMQPClient } from "../../contracts/amqp-client";
 
-export class DeletarProdutoService implements DeletarProdutoUseCase {
+export class DeletarEstoqueService implements DeletarEstoqueUseCase {
     constructor (private readonly amqpClient: AMQPClient) {}
     async deletar(id: number): Promise<void | Error> {
-        const response = await this.amqpClient.send('deletar-produto', { id })
+        const response = await this.amqpClient.send('deletar-estoque', { id })
         if (response['error']) {
             return new Error(response['error'])
         }
