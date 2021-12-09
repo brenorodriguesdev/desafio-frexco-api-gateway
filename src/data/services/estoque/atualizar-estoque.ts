42e0ec31bbaadd409f0be828d824dda4e837ad2a
“Breno <brenorodriguesdev@gmail.com>
2021-12-09 12:21:51 -0300

    feat: atualizar estoque service

---
+++ src/data/services/estoque/atualizar-estoque.ts
@@ -0,0 +1,13 @@
+import { EstoqueModel } from "../../../domain/models/estoque/estoque";
+import { AtualizarEstoqueUseCase } from "../../../domain/useCases/estoque/atualizar-estoque";
+import { AMQPClient } from "../../contracts/amqp-client";
+
+export class AtualizarEstoqueService implements AtualizarEstoqueUseCase {
+    constructor (private readonly amqpClient: AMQPClient) {}
+    async atualizar(data: EstoqueModel): Promise<void | Error> {
+        const response = await this.amqpClient.send('atualizar-estoque', data)
+        if (response['error']) {
+            return new Error(response['error'])
+        }
+    }
+}
