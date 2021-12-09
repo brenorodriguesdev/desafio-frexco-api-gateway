fef23f164ffba852500964c7586f5e0e89cbe8fd
“Breno <brenorodriguesdev@gmail.com>
2021-12-09 12:54:13 -0300

    feat: add atualizar produto controller factory

---
+++ src/main/factories/controllers/produto/atualizar-produto.ts
@@ -0,0 +1,11 @@
+import { AtualizarProdutoService } from "../../../../data/services/produto/atualizar-produto"
+import { CriarProdutoService } from "../../../../data/services/produto/criar-produto"
+import { AMQPClientAdapter } from "../../../../infra/amqp-client"
+import { Controller } from "../../../../presentation/contracts/controller"
+import { AtualizarProdutoController } from "../../../../presentation/controllers/produto/atualizar-produto"
+import { makeAtualizarProdutoValidator } from "../../validators/produto/atualizar-produto"
+
+export const makeAtualizarProdutoController = (): Controller => {
+    const atualizarProdutoService = new AtualizarProdutoService(new AMQPClientAdapter())
+    return new AtualizarProdutoController(makeAtualizarProdutoValidator(), atualizarProdutoService)
+}
