d981ef800e970b3f3da25235c3e76f3f01127630
“Breno <brenorodriguesdev@gmail.com>
2021-12-10 22:34:50 -0300

    feat: add buscar produto route

--- src/main/routes/produto.ts
+++ src/main/routes/produto.ts
@@ -1,6 +1,7 @@
 import { Router } from "express"
 import { adaptRouter } from "../adapters/express-controller"
 import { makeAtualizarProdutoController } from "../factories/controllers/produto/atualizar-produto"
+import { makeBuscarProdutoController } from "../factories/controllers/produto/buscar-produto"
 import { makeCriarProdutoController } from "../factories/controllers/produto/criar-produto"
 import { makeDeletarProdutoController } from "../factories/controllers/produto/deletar-produto"
 import { makeListarProdutosController } from "../factories/controllers/produto/listar-produtos"
@@ -10,4 +11,5 @@ export default (router: Router): void => {
     router.post('/criarProduto', adaptRouter(makeCriarProdutoController()))
     router.delete('/deletarProduto/:id', adaptRouter(makeDeletarProdutoController()))
     router.get('/listarProdutos', adaptRouter(makeListarProdutosController()))
+    router.get('/buscarProduto/:id', adaptRouter(makeBuscarProdutoController()))
 }
