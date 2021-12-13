2539a8cd8aa7f6e29e66d8f5f57e765f0b779e90
“Breno <brenorodriguesdev@gmail.com>
2021-12-13 05:21:45 -0300

    feat: add listar produto estoque por estoque route

--- src/main/routes/produtoEstoque.ts
+++ src/main/routes/produtoEstoque.ts
@@ -1,9 +1,11 @@
 import { Router } from "express"
 import { adaptRouter } from "../adapters/express-controller"
+import { makeListarProdutoEstoquePorEstoqueController } from "../factories/controllers/estoque/listar-produto-estoque-por-estoque"
 import { makeAdicionarProdutoEstoqueController } from "../factories/controllers/produto/adicionar-produto-estoque"
 import { makeDeletarProdutoEstoqueController } from "../factories/controllers/produto/deletar-produto-estoque"
 
 export default (router: Router): void => {
     router.post('/adicionarProdutoEstoque', adaptRouter(makeAdicionarProdutoEstoqueController()))
     router.delete('/deletarProdutoEstoque/:idProduto/:idEstoque', adaptRouter(makeDeletarProdutoEstoqueController()))
+    router.get('/listarProdutoEstoquePorEstoque/:idEstoque', adaptRouter(makeListarProdutoEstoquePorEstoqueController()))
 }
