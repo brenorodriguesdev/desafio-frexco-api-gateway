526edc7cc25b30803c99ec94aab46bf724694709
“Breno <brenorodriguesdev@gmail.com>
2021-12-13 01:24:36 -0300

    feat: add deletar produto estoque service

---
+++ src/data/services/produto/deletar-produto-estoque.ts
@@ -0,0 +1,13 @@
+import { AdicionarProdutoEstoqueModel } from "../../../domain/models/produto/adicionar-produto-estoque";
+import { DeletarProdutoEstoqueUseCase } from "../../../domain/useCases/estoque/deletar-produto-estoque";
+import { AMQPClient } from "../../contracts/amqp-client";
+
+export class DeletarProdutoEstoqueService implements DeletarProdutoEstoqueUseCase {
+    constructor(private readonly amqpClient: AMQPClient) { }
+    async deletar(data: AdicionarProdutoEstoqueModel): Promise<void | Error> {
+        const response = await this.amqpClient.send('deletar-produtoEstoque', data)
+        if (response && response['error']) {
+            return new Error(response['error'])
+        }
+    }
+}
