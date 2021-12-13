481101a8161d08cc906be0622d46d27e9aa82ce2
“Breno <brenorodriguesdev@gmail.com>
2021-12-13 04:44:34 -0300

    feat: add buscar estoque route

--- src/main/routes/estoque.ts
+++ src/main/routes/estoque.ts
@@ -1,6 +1,7 @@
 import { Router } from "express"
 import { adaptRouter } from "../adapters/express-controller"
 import { makeAtualizarEstoqueController } from "../factories/controllers/estoque/atualizar-estoque"
+import { makeBuscarEstoqueController } from "../factories/controllers/estoque/buscar-estoque"
 import { makeCriarEstoqueController } from "../factories/controllers/estoque/criar-estoque"
 import { makeDeletarEstoqueController } from "../factories/controllers/estoque/deletar-estoque"
 import { makeListarEstoquesController } from "../factories/controllers/estoque/listar-estoques"
@@ -10,4 +11,5 @@ export default (router: Router): void => {
     router.post('/criarEstoque', adaptRouter(makeCriarEstoqueController()))
     router.delete('/deletarEstoque/:id', adaptRouter(makeDeletarEstoqueController()))
     router.get('/listarEstoques', adaptRouter(makeListarEstoquesController()))
+    router.get('/buscarEstoque/:id', adaptRouter(makeBuscarEstoqueController()))
 }
